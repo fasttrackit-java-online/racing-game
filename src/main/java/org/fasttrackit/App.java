eeda4f79040f5c577379a286f19b48587aaa1bb2
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-04-09 17:18:36 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+
+// one-line comment
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
