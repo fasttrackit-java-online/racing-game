ea7ab1a61702cab7cc301c82385341bb655a53e8
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-04-16 18:29:06 +0300

    added Car class; studied variables

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Car {
+
+    // instance variables
+    String name = "test";
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+}
