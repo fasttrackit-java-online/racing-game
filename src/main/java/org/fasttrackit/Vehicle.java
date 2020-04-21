f0f8db23155a76cb6461d5b41daf73cdb8f43e43
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-04-21 15:46:18 +0300

    declaring methods

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -11,4 +11,27 @@ public class Vehicle {
 
     boolean damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours) {
+        // Ferrari is accelerating with 100 km/h for 1h
+        System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + "h");
+
+        // local variable
+        double distance = speed * durationInHours;
+
+        totalTraveledDistance = totalTraveledDistance + distance;
+
+        // same result as the statement above
+//        totalTraveledDistance += distance;
+
+        System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
+
+        double usedFuel = distance * mileage / 100;
+
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel);
+
+        return distance;
+    }
+
 }
