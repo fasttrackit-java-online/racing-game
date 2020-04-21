a5b22f9237c47bf221d466ad20ea09cdc87329e2
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-04-21 17:17:30 +0300

    studied conditional statements

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,7 +10,7 @@ public class App {
         carReference.fuelLevel = 60;
         carReference.mileage = 12.5;
         carReference.totalTraveledDistance = 0;
-        carReference.damaged = true;
+        carReference.damaged = false;
         carReference.doorCount = 1;
         carReference.color = "Red";
 
@@ -41,6 +41,8 @@ public class App {
         Car car2 = new Car();
         car2.name = "Renault";
         car2.fuelLevel = 70;
+        car2.maxSpeed = 300;
+        car2.mileage = 10;
         car2.totalTraveledDistance = 100;
 
         car2.engine = new Engine();
@@ -55,9 +57,10 @@ public class App {
         System.out.println("Door Count: " + car2.doorCount);
         System.out.println("Color: " + car2.color);
 
+        System.out.println("*************************************");
         double distanceForCar1 = carReference.accelerate(60, 1);
 
-        double distanceForCar2 = car2.accelerate(100, 1);
+        double distanceForCar2 = car2.accelerate(130, 1);
 
         System.out.println("Engine manufacturer before repair: " + carReference.engine.manufacturer);
         Mechanic mechanic = new Mechanic();
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -13,7 +13,20 @@ public class Vehicle {
     String color;
 
     public double accelerate(double speed, double durationInHours) {
-        // Ferrari is accelerating with 100 km/h for 1h
+        if (fuelLevel <= 0 || damaged) {
+            System.out.println("You cannot accelerate.");
+            return 0;
+        }
+
+        if (speed > maxSpeed) {
+            System.out.println("Max speed exceeded.");
+            return 0;
+        } else if (speed == maxSpeed) {
+            System.out.println("Careful, max speed reached.");
+        } else {
+            System.out.println("Valid speed entered.");
+        }
+
         System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + "h");
 
         // local variable
@@ -26,7 +39,13 @@ public class Vehicle {
 
         System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
 
-        double usedFuel = distance * mileage / 100;
+        double mileageMultiplier = 1;
+
+        if (speed > 120) {
+            mileageMultiplier = speed / 100;
+        }
+
+        double usedFuel = distance * mileage * mileageMultiplier / 100;
 
         fuelLevel -= usedFuel;
         System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel);
