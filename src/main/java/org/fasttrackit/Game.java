6396238fe2e4cec1a2a2094ffae2b5ce221bbce7
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-04-23 16:47:22 +0300

    studied static methods and final variables (constants)

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -4,8 +4,6 @@ import org.fasttrackit.utils.ScannerUtils;
 
 public class Game {
 
-    ScannerUtils scannerUtils = new ScannerUtils();
-
     public void start() {
         System.out.println("Welcome to the Racing Game!");
 
@@ -20,11 +18,11 @@ public class Game {
 
     private int getPlayerCountFromUser() {
         System.out.println("Please enter number of players:");
-        return scannerUtils.nextIntAndMoveToNextLine();
+        return ScannerUtils.nextIntAndMoveToNextLine();
     }
 
     private String getVehicleNameFromUser() {
         System.out.println("Please enter vehicle name:");
-        return scannerUtils.nextLine();
+        return ScannerUtils.nextLine();
     }
 }
