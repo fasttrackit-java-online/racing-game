537a0495745e548a048dacf03f461989c031d41f
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-05-08 16:11:09 +0300

    added Map example

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -13,6 +13,21 @@ public class App {
         Game game = new Game();
         game.start();
 
+//        String userLanguage = "ro";
+//
+//        Map<String, String> descriptionTranslations = new HashMap<>();
+//        descriptionTranslations.put("en", "Some description");
+//        descriptionTranslations.put("ro", "O descriere");
+//        descriptionTranslations.put("ro", "O alta descriere");
+//
+//        String description = descriptionTranslations.get(userLanguage);
+//        System.out.println(description);
+//
+//        System.out.println("Available translations");
+//        for (Map.Entry<String, String> entry : descriptionTranslations.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
+
 //        Set<String> cities = new HashSet<>();
 //        cities.add("Cluj-Napoca");
 //        cities.add("Bucuresti");
