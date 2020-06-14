835548b4a4fbc4b06e527933e1bf6f9765566784
Flaviu Ratiu <fasttrackit.java.online@gmail.com>
2020-06-14 14:29:43 +0300

    added String Pool and object comparison examples

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,8 +1,37 @@
 package org.fasttrackit;
 
+import org.fasttrackit.competitor.vehicle.Car;
+import org.fasttrackit.competitor.vehicle.Engine;
+
 public class App {
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
+
+        // String pool
+//        String a = "test";
+//        String b = "test";
+//        String c = new String("test");
+//
+//        System.out.println("a == b: " + (a == b));
+//        System.out.println("a == c: " + (a == c));
+//        System.out.println("a.equals(c): " + (a.equals(c)));
+
+//        Car car1 = new Car();
+//        car1.setName("Ferrari");
+//
+//        Car car2 = new Car();
+//        car2.setName("Ferrari");
+//
+//        System.out.println(car1.equals(car2));
+
+//        Engine engine1 = new Engine();
+//        engine1.setCapacity(2000);
+//
+//        Engine engine2 = new Engine();
+//        engine2.setCapacity(2000);
+//
+//        System.out.println(engine1.equals(engine2));
+
     }
 }
